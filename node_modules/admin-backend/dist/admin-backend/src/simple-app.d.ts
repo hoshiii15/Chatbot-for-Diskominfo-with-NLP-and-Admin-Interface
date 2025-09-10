b23f79b9f9f5c30cfc2d6d65309cae086971dd98
Hoshiii15 <hosearakan1226@gmail.com>
2025-09-10 08:56:28 +0700

    Add .gitignore and stop tracking node_modules/.next

--- node_modules/admin-backend/dist/admin-backend/src/simple-app.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=simple-app.d.ts.map
--- node_modules/admin-backend/dist/admin-backend/src/utils/logger.d.ts
+++
@@ -1,5 +0,0 @@
-import winston from 'winston';
-export declare const logger: winston.Logger;
-export declare const expressLogger: winston.Logger;
-export default logger;
-//# sourceMappingURL=logger.d.ts.map
